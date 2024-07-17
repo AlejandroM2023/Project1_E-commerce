ec011fc7feea4c50dc72ac665d7f9a225ce94688
AlejandroM2023 <alejandromeza437@gmail.com>
2024-07-17 10:46:32 -0500

    javascript and html Ids

--- assets/logic.js
+++ assets/logic.js
@@ -1,16 +1,52 @@
 //global variables
 
-//build cards here
-function addCards (){};
-//converting currency
-function convertCurrency (){};
+//add button
+const addBtn = document.querySelector('#addBtn');
+//currency selector 
+const currencySlct = document.querySelector('selector');
+//catergory selectors
+const ctgr1 = document.querySelector('#category1');
+const ctgr2 = document.querySelector('#category2');
+const ctgr3 = document.querySelector('#category3');
 
-//functions to load as soon as page loads 
-window.onload=function load (){
-  //function addCards ();
+//array for adding cards and converting currency
+const cardsArr = [];
+
+
+
+//build cards here
+function addCards (){
+  
+};
+
+function sortCategory(){
 
 };
 
+
+
+//converting currency
+function convertCurrency (){
+
+};
+
+
+//functions to load as soon as page loads 
+window.onload=function load (){
+  
+  //event listners currency,addbtn,category
+  
+
+  //addcards
+
+
+};
+
+
+
+
+
 fetch('https://fakestoreapi.com/products?limit=21')
 .then(res=>res.json())
-.then(json=>console.log(json))
+.then()
+
