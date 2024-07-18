19e373e7b0493d141930a5c8c66ef2c0bc88c87d
AlejandroM2023 <alejandromeza437@gmail.com>
2024-07-18 10:53:06 -0500

    fix

--- assets/logic.js
+++ assets/logic.js
@@ -12,78 +12,86 @@ const ctgr4 = document.querySelector('#category4');
 
 //array for adding cards and converting currency
 const cardsArr = [];
+let allProducts;
+let numItems =5;
 
-
+async function getAllProducts() {
+  try {
+    const response = await fetch('https://fakestoreapi.com/products');
+    allProducts = await response.json();
+    addCards();
+  } catch (error) {
+    console.error('Error fetching products:', error);
+  }
+}
 
 //build cards here
-function addCards (){
+async function addCards (){
   const displayArea = document.querySelector('#displayCards');
-  fetch('https://fakestoreapi.com/products?limit=5')
-    .then(function(response){
-      return response.json();
-    })
-    .then(function(data){
-        for(let item of data){
+  for(let i = numItems-5; i<numItems; i++){
           
-          //create elements for product cards
-          const colDiv = document.createElement('div');
-          const cardDiv = document.createElement('div');
-          const cardContentDiv = document.createElement('div');
-          const actionDiv = document.createElement('div');
-          const cardImage = document.createElement('div');
+  //create elements for product cards
+    const colDiv = document.createElement('div');
+    const cardDiv = document.createElement('div');
+    const cardContentDiv = document.createElement('div');
+    const actionDiv = document.createElement('div');
+    const cardImage = document.createElement('div');
 
-          //add materialize classes to div for styling
-          colDiv.setAttribute('class','col s12 m6');
-          cardDiv.setAttribute('class','card large');
-          cardImage.setAttribute('class','card-image');
-          cardContentDiv.setAttribute('class','card-content');
-          actionDiv.setAttribute('class','card-action')
+    //add materialize classes to div for styling
+    colDiv.setAttribute('class','col s12 m6');
+    cardDiv.setAttribute('class','card large');
+    cardImage.setAttribute('class','card-image');
+    cardContentDiv.setAttribute('class','card-content');
+    actionDiv.setAttribute('class','card-action')
 
 
-          //info tags
-          const productImg = document.createElement('img');
-          const productName = document.createElement('span');
-          const productPrice = document.createElement('p');
+    //info tags
+    const productImg = document.createElement('img');
+    const productName = document.createElement('span');
+    const productPrice = document.createElement('p');
 
+
+    //class for title
+    productName.setAttribute('class','card-title');
+
+    //add values to elements
+    productImg.src = allProducts[i].image;
+    productName.textContent = allProducts[i].title;
+    productPrice.textContent = allProducts[i].price;
+
+    //image sizing
+    productImg.setAttribute('class','image-size');
     
-          //class for title
-          productName.setAttribute('class','card-title');
+    //add id to price p tag for access in convert currency
+    productPrice.setAttribute('id','price');
 
-          //add values to elements
-          productImg.src = item.image;
-          productName.textContent = item.title;
-          productPrice.textContent = item.price;
-          
-          //add id to price p tag for access in convert currency
-          productPrice.setAttribute('id','price');
+    //for accesing category
+    colDiv.setAttribute('data-category',allProducts[i].category);
+    
+    //append divs in order
+    colDiv.appendChild(cardDiv);
+    cardDiv.appendChild(cardImage);
+    cardDiv.appendChild(cardContentDiv);
+    cardDiv.appendChild(actionDiv);
 
-          //for accesing category
-          colDiv.setAttribute('data-category',item.category);
-          
-          //append divs in order
-          colDiv.appendChild(cardDiv);
-          cardDiv.appendChild(cardImage);
-          cardDiv.appendChild(cardContentDiv);
-          cardDiv.appendChild(actionDiv);
-
-          //append information
-          cardImage.appendChild(productImg);
-          cardContentDiv.appendChild(productName);
-          cardContentDiv.appendChild(productPrice);
+    //append information
+    cardImage.appendChild(productImg);
+    cardContentDiv.appendChild(productName);
+    cardContentDiv.appendChild(productPrice);
 
 
 
-          //append to card display
-          displayArea.appendChild(colDiv);
+    //append to card display
+    displayArea.appendChild(colDiv);
 
-          //add to card array
-          cardsArr.push(colDiv);
+    //add to card array
+    cardsArr.push(colDiv);
 
 
-        }
-    });
+  }
+  numItems +=5;
+}
   
-};
 
 function sortCategory(event){
   displayCards.innerHTML='';
@@ -98,22 +106,20 @@ function sortCategory(event){
 
 
 //converting currency
-async function fetchConversion(formCurrency, toCurrency, amount) {
-  const url = 'https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=AUD&to=CAD&amount=1&language=en';
-  const options = {
-    method: 'GET',
-    headers: {
-      'x-rapidapi-key': '3be8c18d27msh23a18245b829566p1e9860jsn17e38541da51',
-      'x-rapidapi-host': 'currency-converter5.p.rapidapi.com'
-    }
-  };
-  
+async function fetchConversionRate(fromCurrency, toCurrency) {
   try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
+    const response = await fetch(`https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=${fromCurrency}&to=${toCurrency}&amount=1`, {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'YOUR_RAPIDAPI_KEY', // Replace API key
+        'X-RapidAPI-Host': 'currency-converter5.p.rapidapi.com'
+      }
+    });
+    const data = await response.json();
+    return data.rates[toCurrency].rate;
   } catch (error) {
-    console.error(error);
+    console.error('Error fetching conversion rate:', error);
+    return 1; // Default to 1 if there's an error
   }
 }
 //functions to load as soon as page loads 
@@ -121,8 +127,7 @@ async function fetchConversion(formCurrency, toCurrency, amount) {
 
 //functions to load as soon as page loads 
 window.onload=function load (){
-  
-  addCards();
+  getAllProducts();
   
 //event listners addbtn
   addBtn.addEventListener('click',function(){
